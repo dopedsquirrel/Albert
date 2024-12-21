63e692bf41236ce8277579b5ac21789b9d347a44
dopedsquirrel <86486395+dopedsquirrel@users.noreply.github.com>
2024-12-21 22:51:59 +0000

    changed audio path

--- recognize.py
+++ recognize.py
@@ -5,7 +5,7 @@ import os
 
 # Konfiguration
 MODEL_PATH = "model"  # Pfad zum Vosk-Modell
-AUDIO_PATH = "albert-message.wav"  # Pfad zur Weihnachtsnachricht
+AUDIO_PATH = "/app/albert-message.wav"  # Pfad zur Weihnachtsnachricht
 SAMPLE_RATE = 16000   # Abtastrate
 CHUNK_SIZE = 4000     # Größe der Audio-Chunks
 
